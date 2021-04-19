ae5c020e9a15a1088a157641d7ecb3aa0bab45c0
OliveiraLeonardo17 <oliveira.leonardo.r@gmail.com>
2021-04-19 20:39:26 -0300

    Terraform format

--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
 variable "virtual_machine_name" {
-  type = string
+  type        = string
   description = "(Required) The name of the Linux Virtual Machine. Changing this forces a new resource to be created."
-  default = "example-vm"
+  default     = "example-vm"
 }
 
 variable "virtual_machine_size" {
-  type = string
+  type        = string
   description = "(Required) The SKU which should be used for this Virtual Machine, such as Standard_DS2_v2."
-  default = "Standard_F2"
+  default     = "Standard_F2"
 }
 
 variable "virtual_admin_username" {
-  type = string
+  type        = string
   description = "(Required) The username of the local administrator used for the Virtual Machine. Changing this forces a new resource to be created."
-  default = "adminuser"
+  default     = "adminuser"
 }

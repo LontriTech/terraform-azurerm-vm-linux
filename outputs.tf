429e0463a0a50800b1c6c13262a7c302dca290aa
OliveiraLeonardo17 <oliveira.leonardo.r@gmail.com>
2021-04-26 21:51:38 -0300

    output applied_dns_servers

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,8 @@ output "network_interface_private_ip" {
   description = "Private IP address of the deployed VM's NIC."
   value       = azurerm_network_interface.nic.private_ip_address
 }
+
+output "applied_dns_servers" {
+  description = "If the Virtual Machine using this Network Interface is part of an Availability Set, then this list will have the union of all DNS servers from all Network Interfaces that are part of the Availability Set."
+  value       = azurerm_network_interface.nic.applied_dns_servers
+}

5536cd71297b5ac34729cf025c33e1a6b7d740e2
OliveiraLeonardo17 <oliveira.leonardo.r@gmail.com>
2021-05-02 17:13:47 -0300

    fix example-requirements outputs reference

--- modules/example-requirements/outputs.tf
+++ modules/example-requirements/outputs.tf
@@ -5,10 +5,10 @@ output "virtual_network_name" {
 
 output "resource_group_name" {
   description = "The name of the resource group in which the virtual network was deployed."
-  value       = azurerm_virtual_network.vnet.resource_group_name
+  value       = azurerm_virtual_network.subnet.resource_group_name
 }
 
 output "subnet_name" {
   description = "The name of the subnet."
-  value       = azurerm_virtual_network.vnet.name
+  value       = azurerm_virtual_network.subnet.name
 }

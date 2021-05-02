fd3b8acfe68f3fa6b92b77fed4d2512098d2be2b
OliveiraLeonardo17 <oliveira.leonardo.r@gmail.com>
2021-05-02 17:33:19 -0300

    fix examples/basic resource_group_name ref

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -2,7 +2,7 @@ module "example-vm-linux-basic" {
   source = "../.."
 
   virtual_machine_name = "example"
-  resource_group       = module.example_requirements.resource_group_name
+  resource_group_name  = module.example_requirements.resource_group_name
   virtual_network_name = module.example_requirements.virtual_network_name
   subnet_name          = module.example_requirements.subnet_name
 }

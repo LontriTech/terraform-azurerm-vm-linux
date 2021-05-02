046f0c774e40a1a80b6ba5ab288133276fd52573
OliveiraLeonardo17 <oliveira.leonardo.r@gmail.com>
2021-05-02 17:23:16 -0300

    fix ref to rg name on root module's main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ data "azurerm_resource_group" "resource_group" {
 data "azurerm_subnet" "subnet" {
   name                 = var.subnet_name
   virtual_network_name = var.virtual_network_name
-  resource_group_name  = data.resource_group_name
+  resource_group_name  = data.azurerm_resource_group.resource_group.name
 }
 
 locals {
